fbb861e18057bef6a1c8b794f2afeb4aed875d0d
Andrey Uzun <uzun.andrei13@gmail.com>
2025-10-28 07:26:34 +0200

    added reading json files

--- lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
@@ -1,4 +1,12 @@
+package oop.practice;
+import java.io.Reader;
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 class Universe {
     private final String name;
@@ -18,6 +26,57 @@ class Universe {
     }
 }
 
+class Files {
+    void createFile() {
+        File file = new  File("lab-papers-please\\java-classifcation\\src\\main\\resources\\input.json");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.toPath().getFileName());
+            }
+            else  {
+                System.out.println("File already exists: " + file.toPath().getFileName());
+            }
+        }
+        catch(IOException e) {
+            System.out.println("File not found: " + file.toPath().getFileName());
+            e.printStackTrace();
+        }
+    }
+    void readFile(File obj) {
+        try (Scanner  scanner = new Scanner(obj)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println("Data: ");
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + obj.toPath().getFileName());
+            e.printStackTrace();
+        }
+    }
+
+    void readJsonFile(File obj) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(obj);
+            System.out.println("Full JSON: ");
+            System.out.println(root.toPrettyString());
+
+            if (root.isArray()) {
+                System.out.println("Entries: ");
+                for (JsonNode node : root) {
+                    System.out.println(node);
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error reading file: " + obj.toPath().getFileName());
+            e.printStackTrace();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         List<String> uniIndividuals = List.of("Courier 6", "Caesar", "General Oliver", "Mr. House");
@@ -32,5 +91,10 @@ public class Main {
                 System.out.print(", ");
             }
         }
+        Files files = new Files();
+        files.createFile();
+        File fileObj = new File("lab-papers-please\\java-classifcation\\src\\main\\resources\\input.json");
+        // files.readFile(fileObj);
+        files.readJsonFile(fileObj);
     }
 }
