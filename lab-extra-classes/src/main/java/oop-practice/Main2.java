9018fd234887333e169caf8b1cbff47f5406452c
Andrey Uzun <uzun.andrei13@gmail.com>
2025-11-05 20:59:33 +0200

    file reading implemented, class TextData nearly complete

--- lab-extra-classes/src/main/java/oop-practice/Main2.java
+++ lab-extra-classes/src/main/java/oop-practice/Main2.java
@@ -3,9 +3,92 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class FileReader {
-    public void readFileIntoString(String path) {
+    public String readFileIntoString(String path) {
+        var content = new StringBuilder();
+        var file = new File(path);
+        try (var scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                content.append(scanner.nextLine());
+                if (scanner.hasNextLine()) {
+                    content.append("\n");
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found: " + path);
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
+
+class TextData {
+    private String fileName;
+    private String text;
+    private int numberOfVowels;
+    private int numberOfConsonants;
+    private int numberOfLetters;
+    private int numberOfSentences;
+    private String longestWord;
+
+    public TextData(String fileName, String text) {
+        this.fileName = fileName;
+        this.text = text;
+        calculateVowelsAndConsonants();
 
     }
+
+    private void calculateVowelsAndConsonants() {
+        numberOfVowels = 0;
+        numberOfConsonants = 0;
+        var lowerText = text.toLowerCase();
+
+        for (int i = 0; i < lowerText.length(); i++) {
+            var c = lowerText.charAt(i);
+
+            if (Character.isLetter(c)) {
+                if (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i') {
+                    numberOfVowels++;
+                }
+                else {
+                    numberOfConsonants++;
+                }
+            }
+        }
+
+    }
+
+    private void calculateLetters() {
+        numberOfLetters = 0;
+        for (int i = 0; i < text.length(); i++) {
+            var c = text.charAt(i);
+            if (Character.isLetter(c)) {
+                numberOfLetters++;
+            }
+        }
+    }
+    private void calculateSentences() {
+        numberOfSentences = 0;
+        for (int i = 0; i < text.length(); i++) {
+            var c = text.charAt(i);
+            if (c == '!' || c == '.' || c == '?') {
+                numberOfSentences++;
+            }
+        }
+    }
+
+    private void findLongestWord() {
+        longestWord = "";
+        var words = text.split("\\s+");
+
+        for (var word : words) {
+            var cleanedWord = word.replaceAll("[^a-zA-Z]","");
+            if (cleanedWord.length() > longestWord.length()) {
+                longestWord = cleanedWord;
+            }
+        }
+    }
+
 }
 
 public class Main2 {
